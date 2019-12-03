da25e9d1589188d07d3efd94231122f5a8e26c64
kulakli_cavit@hotmail.com <kulakli_cavit@hotmail.com>
2019-12-03 10:57:01 +0300

    Skeleton of project

---
+++ README.md
@@ -0,0 +1,15 @@
+
+# Watch List 
+
+SwiftUI application for listing movies, books and series.
+
+### Built With 
+
+MacOS Catalina + Xcode 11 or higher version (SwiftUI needs)
+
+```
+com.yourname.WatchList
+```
+
+Set your team and build
+
---
+++ WatchList/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchList
+//
+//  Created by Cavit KULAKLI on 3.12.2019.
+//  Copyright © 2019 Cavit KULAKLI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
